76abcc51514dd2c8cb6acf122719732a61b2e159
Research Kreat <research@kreat.ai>
2025-07-21 17:18:50 +0530

    Cleaned First 5k

--- Helpers/csv_cleaner.py
+++ Helpers/csv_cleaner.py
@@ -1,7 +1,7 @@
 import csv
 
-input_file = "D:/BNMIT/Semester 7/Final Year Project/scraper/Datasets/merged.csv"
-output_file = "../clean_merged_30k.csv"
+input_file = "cleaned_file.csv"
+output_file = "clean_merged_5k.csv"
 
 seen_rows = set()
 rows_written = 0
